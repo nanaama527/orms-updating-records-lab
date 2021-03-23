2d00c80d1d95038a5830107edb469bea89f7d067
Nana <72888369+nanaama527@users.noreply.github.com>
2021-03-23 16:52:59 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -62,13 +62,8 @@ class Student
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end
-
-
 end
 
 
 
 
-
-# Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
